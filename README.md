e69b4aa00850ce589cfa0e7fb8e624c8502f318b
DragonflyValkyrie <148598429+DragonflyValkyrie@users.noreply.github.com>
2024-03-08 19:46:10 -0500

    More functions to parse sentences
    GPRMC GPVTG GPGLL sentences are now parsed and printed.
    
    Code refactor that converts longitude and latitude to decimal is in a function.
    
    Added more data points in GPS data struct.
    
    Updated README.

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Raspberry Pi Pico(RP2040) GPS NMEA Sentence Parser
  
-This C code is designed to parse NMEA sentences from a GPS module, and extract useful information such as time, latitude, longitude, altitude, speed, course, and fix status. The parsed data is then printed to the console in a human-readable format. Other satallite constelations such as GLONASS, Galileo, or BeiDou would not be parsed as cose only parses GPGGA sentences. Thiis is due because Adafruit Ultimate GPS only recieves GPGGA sentences. The parser can be edited to change format for their identifiers. 
+This C code is designed to parse NMEA sentences from a GPS module, and extract useful information such as time, latitude, longitude, altitude, speed, course, and fix status. The parsed data is then printed to the console in a human-readable format. Other satallite constelations such as GLONASS, Galileo, or BeiDou would not be parsed as this only parses GP sentences. This is due because Adafruit Ultimate GPS only recieves GPGGA sentences. The parser can be edited to change format for other identifiers and structure.
 
-This has been tested with Adafruit Ultimate GPS breakout board and a Pico using C/C++ SDK.
+## Tested
 
-The .vscode is from the Raspberry Pi Foundation Pico Examples to set up the enivorment for windows vscode.
+This has been tested with Adafruit Ultimate GPS breakout board, NEO M8N u-blox gps, NEO 6M u-blox gps and a Pico using C/C++ SDK.
+
+## Configuration
+
+The .vscode is from the Raspberry Pi Foundation Pico Examples to set up the enivorment for windows vscode. This includes debugging with the pico probe.
 
 ## To build
 
--- pico-gps.c
+++ pico-gps.c
@@ -27,12 +27,16 @@ typedef struct {
     float longitudeDegrees; // Longitude in decimal degrees
     float geoidheight;      // Difference between geoid height and WGS84 height
     float altitude;         // Altitude in meters above MSL
-    float speed;            ///< Current speed over ground in knots
-    float angle;            ///< Course in degrees from true north
+    float speedknot;        // Current speed over ground in knots
+    float speedkm;          // Current speed over ground in knots
+    float angle;            // Course in degrees from true north
+    char heading_unit_true; // True (geographic) north
+    char heading_unit_mag;  // Magnetic north
 
     char lat;               // N / S
     char lon;               // E / W
     bool fix;               // GPS fix
+    char mode;              // A = Autonomous , D = DGPS, E = DR (This field is only present in NMEA version 3.0)
     int satellites_in_view; // Satellies in view
 } GPS_Data;
 
@@ -53,11 +57,15 @@ GPS_Data gps_data_default = {
     .longitudeDegrees = 0.0,
     .geoidheight = 0.0,
     .altitude = 0.0,
-    .speed = 0.0,
+    .speedknot = 0.0,
+    .speedkm = 0.0,
     .angle = 0.0,
     .lat = 'X',
     .lon = 'X',
     .fix = false,
+    .mode = 'X',
+    .heading_unit_true = 'X',
+    .heading_unit_mag = 'X',
     .satellites_in_view = 0
 };
 
@@ -83,15 +91,8 @@ bool is_valid_nmea_sentence(const char *sentence) {
     return checksum == sentence_checksum;
 }
 
-void parse_GPGGA(const char *sentence, GPS_Data *data) {
-    // Assuming a valid GPGGA sentence format: $GPGGA,hhmmss.sss,llll.lll,a,yyyyy.yyy,b,xx,yy.y,x.x,x.x,M,x.x,M,x.x,xxxx*hh\r\n
-
-    // Parse the sentence using sscanf, skipping fields related to HDOP, geoidal separation, and age of DGPS data
-    int result = sscanf(sentence, "$GPGGA,%2hhu%2hhu%2hhu.%3hu,%f,%c,%f,%c,%1hhu,%d,,,,*%2hhx",
-                        &data->hour, &data->minute, &data->seconds, &data->milliseconds,
-                        &data->latitude, &data->lat, &data->longitude, &data->lon,
-                        &data->fix, &data->satellites_in_view);
-
+// Convert latitude and longitude to decimal degrees
+void convert_coords_to_decimal(GPS_Data *data) {
     // Calculate decimal degrees for latitude and longitude
     data->latitudeDegrees = (int)data->latitude / 100 + (data->latitude - (int)data->latitude) / 60;
     data->longitudeDegrees = (int)data->longitude / 100 + (data->longitude - (int)data->longitude) / 60;
@@ -105,19 +106,57 @@ void parse_GPGGA(const char *sentence, GPS_Data *data) {
     }
 }
 
+void parse_GPGGA(const char *sentence, GPS_Data *data) {
+    // GPGGA sentence format: $GPGGA, HHMMSS.sss, ddmm.mmmm, N/S, dddmm.mmmm, E/W, 0-2, 0-12, x.x, x.x, M, x.x, M, x.x, xxxx*cs\r\n
+
+    // Parse the sentence, skipping fields related to HDOP, geoidal separation, and age of DGPS data
+    int result = sscanf(sentence, "$GPGGA,%2hhu%2hhu%2hhu.%3hu,%f,%c,%f,%c,%1hhu,%d,,,,*%2hhx",
+                        &data->hour, &data->minute, &data->seconds, &data->milliseconds,
+                        &data->latitude, &data->lat, &data->longitude, &data->lon,
+                        &data->fix, &data->satellites_in_view);
+
+    convert_coords_to_decimal(data);
+}
+
 void parse_GPGLL(const char *sentence, GPS_Data *data) {
+    // GPGLL sentence format: $GPGLL, latitude, N/S, longitude, E/W, time, A, *cs\r\n
 
+    char fix_indicator; // Use char to represent 'A' or 'V'
 
+    // Parse the sentence using sscanf, skipping mode field
+    int result = sscanf(sentence, "$GPGLL,%f,%c,%f,%c,%2hhu%2hhu%2hhu.%3hu,%c,%c,*%2hhx",
+                        &data->latitude, &data->lat, &data->longitude, &data->lon,
+                        &data->hour, &data->minute, &data->seconds, &data->milliseconds,
+                        &fix_indicator, &data->mode);
+
+    // Convert 'A' to true, 'V' to false and assign to data->fix
+    data->fix = (fix_indicator == 'A') ? true : false;
+
+    convert_coords_to_decimal(data);
 }
 
 void parse_GPVTG(const char *sentence, GPS_Data *data) {
-
+    // GPVTG sentence format: $GPVTG, course, reference, course, reference, speed over ground, N, speed over ground, K, mode*cs\r\n
+    
+    int result = sscanf(sentence, "$GPVTG,%f,%c,,%*c,%f,%*c,%f,%*c,%c,*%2hhx",
+                        &data->angle, &data->heading_unit_true, &data->speedknot, &data->speedkm, &data->mode);
 
 }
 
 void parse_GPRMC(const char *sentence, GPS_Data *data) {
+    // GPRMC sentence format: $GPVTG, time, status, latitude, N/S, longitude, E/W, speed over ground, course over ground, date, magnetic variation, mode*cs\r\n
 
+    char fix_indicator; // Use char to represent 'A' or 'V'
 
+    int result = sscanf(sentence, "$GPRMC,%2hhu%2hhu%2hhu.%3hu,%c,%f,%c,%f,%c,%f,%f,%2hhu%2hhu%2hhu,,,%c,*%2hhx",
+                        &data->hour, &data->minute, &data->seconds, &data->milliseconds, &fix_indicator,
+                        &data->latitude, &data->lat, &data->longitude, &data->lon, &data->speedknot,
+                        &data->angle, &data->day, &data->month, &data->year, &data->mode);
+
+    // Convert 'A' to true, 'V' to false and assign to data->fix
+    data->fix = (fix_indicator == 'A') ? true : false;
+
+    convert_coords_to_decimal(data);
 }
 
 void parse_GPGSA(const char *sentence, GPS_Data *data) {
@@ -125,7 +164,7 @@ void parse_GPGSA(const char *sentence, GPS_Data *data) {
 
 }
 
-void parse_GPSV(const char *sentence, GPS_Data *data) {
+void parse_GPGSV(const char *sentence, GPS_Data *data) {
 
 
 
@@ -133,29 +172,32 @@ void parse_GPSV(const char *sentence, GPS_Data *data) {
 
 // Function to print GPS data
 void print_gps_data(const GPS_Data *data) {
-    // Print each field of the GPS_Data structure
     printf("Time: %02d:%02d:%02d.%03d\n", data->hour, data->minute, data->seconds, data->milliseconds);
 
-    // Print Latitude in Degrees/Minutes format
+    printf("DDMMYYY Date: %02d/%02d/20%02d\n", data->day, data->month, data->year);
+
     printf("Latitude (Degrees/Minutes): %.4f %c\n", data->latitude, data->lat);
 
-    // Print Longitude in Degrees/Minutes format
     printf("Longitude (Degrees/Minutes): %.4f %c\n", data->longitude, data->lon);
 
-    // Print Latitude in Decimal Degrees format
     printf("Latitude (Decimal Degrees): %.6f %c\n", data->latitudeDegrees, data->lat);
 
-    // Print Longitude in Decimal Degrees format
     printf("Longitude (Decimal Degrees): %.6f %c\n", data->longitudeDegrees, data->lon);
 
     printf("Altitude: %.2f meters\n", data->altitude);
-    printf("Speed: %.2f knots\n", data->speed);
+
+    printf("Speed: %.2f knots\n", data->speedknot);
+
+    printf("Speed: %.2f km\n", data->speedkm);
+
     printf("Course: %.2f degrees\n", data->angle);
 
-    // Print number of satellites
+    printf("Heading: %.2f %c\n", data->angle, data->heading_unit_true);
+
+    printf("Mode: %c\n", data->mode);
+
     printf("Satellites in view: %d\n", data->satellites_in_view);
 
-    // Example: Print fix status
     if (data->fix) {
         printf("Fix: Yes\n");
     } else {
@@ -163,7 +205,8 @@ void print_gps_data(const GPS_Data *data) {
     }
 }
 
-int main() {   
+int main() {
+    // Refer to https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf for configurations
     const char configurations[] = "$PMTK314,1,1,1,1,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0*2C\r\n";
 
     stdio_init_all();
@@ -175,7 +218,7 @@ int main() {
 
     uart_puts(UART_ID, configurations);
 
-    char sentence_buffer[256];  // Adjust the size as needed
+    char sentence_buffer[256];
     int sentence_index = 0;
 
     while (1) {
@@ -189,15 +232,15 @@ int main() {
             // Null-terminate the buffer to make it a valid string
             sentence_buffer[sentence_index] = '\0';
 
-            printf(sentence_buffer);
+printf(sentence_buffer);
 
             // Process the NMEA sentence
             if (is_valid_nmea_sentence(sentence_buffer)) {
-                GPS_Data data;
-                parse_GPGGA(sentence_buffer, &data);
-                print_gps_data(&data);
+                if (strncmp(sentence_buffer, "$GPGSA", 6) == 0) {
+                    parse_GPGSA(sentence_buffer, &gps_data_default);
+                    print_gps_data(&gps_data_default);
+                }
             }
-
             // Reset the buffer for the next sentence
             sentence_index = 0;
         }
